fd47d8e714936baab15090d82397884f05d7c418
jenheaton <jea.heaton@gmail.com>
2025-08-23 17:31:40 -0400

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDtPSCkhqwUYwLtjRnB8WciqZKvPz8TakE",
+  authDomain: "baby-shower-game-a2858.firebaseapp.com",
+  databaseURL: "https://baby-shower-game-a2858-default-rtdb.firebaseio.com",
+  projectId: "baby-shower-game-a2858",
+  storageBucket: "baby-shower-game-a2858.firebasestorage.app",
+  messagingSenderId: "323912064205",
+  appId: "1:323912064205:web:d9ae8e7b7f3e5545c35d84"
+};
